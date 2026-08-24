bc1b3c789a4a70235feb9061bbbd73c4f51e6edb
kemalcoskunnn-eng <kemal.bro1903@gmail.com>
2026-08-24 14:29:33 +0300

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://pswsevxfzeqsctfyynqr.supabase.co",
+  SUPABASE_URL: "https://pswsevxfzeqsctfyyngr.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_g9BsenQo0ERYMaoh3u7RCw_lOM6f_hn"
 };
